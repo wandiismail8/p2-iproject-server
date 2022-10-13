188d7adbbccafa0215b017c3681215ce257a504e
wandiismail8 <wandiismail8@gmail.com>
2022-10-13 11:26:12 +0700

    Deploy

--- server/app.js
+++ server/app.js
@@ -1,9 +1,15 @@
+
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+  }
+  
 const express = require("express");
 const cors = require("cors");
 const router = require("./routes");
 const errorHandler = require("./middleware/errorHandler");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
